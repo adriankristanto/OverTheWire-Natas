ddb7f88c5b618e9d911aa0f2ef3f405b0aec683f
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-25 16:30:52 +1000

    added the guide for natas1 -> natas2

--- natas1/natas1.py
+++ natas1/natas1.py
@@ -10,3 +10,15 @@ URL = "http://natas1.natas.labs.overthewire.org"
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 
+response = requests.get(url=URL, auth=AUTH)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+body = soup.find('body')
+div_content = body.find('div', {'id': 'content'})
+comment = div_content.find(text=lambda line: isinstance(line, bs4.Comment))
+
+
+# natas2 password: ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi
+password = re.search(r'is (\w+) ', comment).group(1)
+print(f"natas2 password: {password}")
