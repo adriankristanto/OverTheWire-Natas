533fd1165cd9013055fa51b2930a04b4b733ae04
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-25 16:26:01 +1000

    start with natas1 -> natas2

---
+++ natas1/natas1.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas1
+USERNAME = "natas1"
+PASSWORD = "gtVrDuiDfck831PqWsLEZy5gyDz1clto"
+URL = "http://natas1.natas.labs.overthewire.org"
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
