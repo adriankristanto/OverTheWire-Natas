90462797b8c623d1c5937696bcb1110c57a3ac85
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-25 16:04:45 +1000

    added the guide for natas0 -> natas1

--- natas0/natas0.py
+++ natas0/natas0.py
@@ -1,6 +1,33 @@
 import requests
 import re
+import bs4
 
+
+# configuration for natas0
 USERNAME = "natas0"
 PASSWORD = "natas0"
+URL = 'http://natas0.natas.labs.overthewire.org'
+# for simple http authentication
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
+
+# get the page source
+response = requests.get(url=URL, auth=AUTH)
+# pass the page source to BeautifulSoup
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+# the header doesn't seem relevant to the challenge as stated by the following comment
+# <!-- This stuff in the header has nothing to do with the level -->
+body = soup.find('body')
+# the comment is contained within the div with id=content
+div_content = body.find('div', {'id': 'content'})
+# create a lambda function to get the comment containing the password
+# within the body element
+# reference: https://stackoverflow.com/questions/33138937/how-to-find-all-comments-with-beautiful-soup
+comment = div_content.find(text=lambda line: isinstance(line, bs4.Comment))
+
+
+# natas1 password: gtVrDuiDfck831PqWsLEZy5gyDz1clto
+password = re.findall(r'is (.*) ', comment)[0]
+print(f"natas1 password: {password}")
