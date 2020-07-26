692d0d34dc99534bb6498112d2c2729545277672
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-26 18:59:16 +1000

    search for div element with id=content which contains the challenge

--- natas3/natas3.py
+++ natas3/natas3.py
@@ -8,3 +8,10 @@ USERNAME = 'natas3'
 PASSWORD = 'sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14'
 URL = 'http://natas3.natas.labs.overthewire.org'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
+response = requests.get(url=URL, auth=AUTH)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+body = soup.find('body')
+div_content = body.find('div', {'id' : 'content'})
+print(div_content)
