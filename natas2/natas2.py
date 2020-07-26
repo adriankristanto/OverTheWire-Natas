287a915135391bbf49f9aa7b58eed11e6615d6f7
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-26 19:17:16 +1000

    make the guide for natas2 -> natas3 more verbose

--- natas2/natas2.py
+++ natas2/natas2.py
@@ -14,6 +14,7 @@ response = requests.get(url=URL, auth=AUTH)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 body = soup.find('body')
 div_content = body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
 # as we can see there is an image, with the src files/pixel.png
 # we can check the files directory
 image = div_content.find('img', {'src' : 'files/pixel.png'})
@@ -23,6 +24,7 @@ response = requests.get(url=URL + '/files', auth=AUTH)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 # there is a link to the file users.txt in the files directory
 users = soup.find('a', {'href' : 'users.txt'})
+print(f'{users}\n')
 
 
 # natas3 password: sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14
