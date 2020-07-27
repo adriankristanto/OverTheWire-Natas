6b5957eff02d5e9f254f78d02f6bbe94540938fb
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-27 12:26:11 +1000

    start with natas4 -> natas5

---
+++ natas4/natas4.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas4
+USERNAME = 'natas4'
+PASSWORD = 'Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ'
+URL = 'http://natas4.natas.labs.overthewire.org'
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
