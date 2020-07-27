2eeaf9573c90c8fb5b8a73500a287f299a62675c
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-27 13:11:07 +1000

    used Session to persist connection

--- natas4/natas4.py
+++ natas4/natas4.py
@@ -10,7 +10,9 @@ URL = 'http://natas4.natas.labs.overthewire.org'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 
-response = requests.get(url=URL, auth=AUTH)
+session = requests.Session()
+session.auth = AUTH
+response = session.get(url=URL)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 div_content = soup.find('body').find('div', {'id' : 'content'})
 print(f'{div_content}\n')
@@ -19,7 +21,8 @@ print(f'{div_content}\n')
 # we need to change the referer in the header request
 # as the site only accepts authorised users from
 # http://natas5.natas.labs.overthewire.org/
-response = requests.get(url=URL, auth=AUTH, headers={'referer' : 'http://natas5.natas.labs.overthewire.org/'})
+session.headers.update({'referer' : 'http://natas5.natas.labs.overthewire.org/'})
+response = session.get(url=URL)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 div_content = soup.find('body').find('div', {'id' : 'content'})
 print(f'{div_content}\n')
