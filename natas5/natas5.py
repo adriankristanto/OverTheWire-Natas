bb3c02a10c178889dae8d9fa3eacf5bac0cbc7f3
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-27 13:08:31 +1000

    used Session to create a persistent connection, i.e. we don't need to supply the authentication object to the second request

--- natas5/natas5.py
+++ natas5/natas5.py
@@ -10,20 +10,24 @@ URL = 'http://natas5.natas.labs.overthewire.org/'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 
-response = requests.get(URL, auth=AUTH)
+session = requests.Session()
+session.auth = AUTH
+response = session.get(URL)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 div_content = soup.find('body').find('div', {'id' : 'content'})
 print(f'{div_content}\n')
 
 
-print(f'{response.cookies}')
+print(f'{response.cookies}\n')
 
 
 # we can't gain access as the site says that we aren't logged in
 # going through the request header, we can see that there is a cookie
 # loggedin=0
 # let's try to set it to 1, i.e. loggedin=1
-response = requests.get(URL, auth=AUTH, cookies={'loggedin' : '1'})
+# reference: https://requests.readthedocs.io/en/master/api/#api-cookies
+requests.utils.add_dict_to_cookiejar(session.cookies, {'loggedin' : '1'})
+response = session.get(URL)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 div_content = soup.find('body').find('div', {'id' : 'content'})
 print(f'{div_content}\n')
