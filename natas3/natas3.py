17caa98864cb09ce0038f04950d0733371a11fd0
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-27 13:12:43 +1000

    used Session to persist connection

--- natas3/natas3.py
+++ natas3/natas3.py
@@ -10,7 +10,9 @@ URL = 'http://natas3.natas.labs.overthewire.org'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 
-response = requests.get(url=URL, auth=AUTH)
+session = requests.Session()
+session.auth = AUTH
+response = session.get(url=URL)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 body = soup.find('body')
 div_content = body.find('div', {'id' : 'content'})
@@ -20,13 +22,13 @@ print(f'{div_content}\n')
 # reference: https://support.google.com/webmasters/answer/6062608?hl=en
 # the comment indicates the existence of robots.txt
 # it says that even the google's crawler won't be able to find the password
-response = requests.get(url=URL + '/robots.txt', auth=AUTH)
+response = session.get(url=URL + '/robots.txt')
 print(f'{response.text}\n')
 
 
 # in robots.txt, we find /s3cr3t/ directory
 s3cr3t = re.search(r'Disallow: (/\w+/)', response.text).group(1)
-response = requests.get(url=URL + s3cr3t, auth=AUTH)
+response = session.get(url=URL + s3cr3t)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 a = soup.find('a', {'href' : 'users.txt'})
 print(f'{a}\n')
@@ -36,6 +38,6 @@ users = re.search(r'>(.+)<', str(a)).group(1)
 
 
 # natas4 password: Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ
-response = requests.get(url=URL + s3cr3t + users, auth=AUTH)
+response = session.get(url=URL + s3cr3t + users)
 password = re.search(r'natas4:(\w+)', response.text).group(1)
 print(f'natas4 password: {password}')
