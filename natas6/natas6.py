a22bce2ad8a2582ffddb28d693cf11ae31ad160d
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-28 12:51:31 +1000

    get the page source

--- natas6/natas6.py
+++ natas6/natas6.py
@@ -11,3 +11,18 @@ URL = 'http://natas6.natas.labs.overthewire.org/'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 
+session = requests.Session()
+session.auth = AUTH
+response = session.get(URL)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+div_content = soup.find('body').find('div', {'id' : 'content'})
+print(f'{div_content}\n')
+
+
+# get source code
+response = session.get(URL + 'index-source.html')
+# the response contains the source code as encoded html file, e.g. &lt; which should've been <br
+# therefore, we can use html.unescape() to decoded the html file
+source = html.unescape(response.text).replace('<br />', '')
+soup = bs4.BeautifulSoup(source, 'lxml')
+print(soup.find('div').prettify())
