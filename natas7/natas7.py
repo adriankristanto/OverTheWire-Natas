8af074fd4dfafd3f0fb58d15f7515cd49998e789
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-30 14:21:14 +1000

    added attack explanation

--- natas7/natas7.py
+++ natas7/natas7.py
@@ -22,3 +22,12 @@ print(f'{div_content}\n')
 response = session.get(URL + 'index.php?page=random')
 div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
 print(f'{div_content}\n')
+# it seems that whatever we pass to 'page' will be passed to php include() function without any input sanitization
+# one attack that we can try is php local file inclusion, where we can access the file that the server has access to
+
+
+# according to the error, the directory structure is /var/www/natas/natas7/index.php
+# to get to /, we need ../../../../
+# natas7 > natas > www > var > / 
+# and the password, mentioned in the hint for this level, is stored in /etc/natas_webpass/natas8
+# therefore, we can use ../../../../etc/natas_webpass/natas8
