33a50147ad1b741b30b8e254b96b516ebe16c769
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-31 22:55:19 +1000

    start with natas8 -> natas9

---
+++ natas8/natas8.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas8
+USERNAME = 'natas8'
+PASSWORD = 'DBfUBfqQG69KvJvJ1iAbMoIpwSNQ9bWe'
+URL = 'http://natas8.natas.labs.overthewire.org/'
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
