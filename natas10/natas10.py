a7f2efae9acc6ee0da4bb20c8a0e89e46f7f2314
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-02 13:24:07 +1000

    get challenge content

--- natas10/natas10.py
+++ natas10/natas10.py
@@ -10,3 +10,8 @@ URL = 'http://natas10.natas.labs.overthewire.org/'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 
+session = requests.Session()
+session.auth = AUTH
+response = session.get(URL)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
