2c75972d2a47dac9465b9add885600e5269a072e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-02 13:22:27 +1000

    start with natas10 -> natas11

---
+++ natas10/natas10.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas10
+USERNAME = 'natas10'
+PASSWORD = 'nOpp1igQAkUzaI1GUUjzn1bFVj7xCNzu'
+URL = 'http://natas10.natas.labs.overthewire.org/'
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
