9e477c42a9db7be62d13c8bc4ef2711e902b9757
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-02 13:28:25 +1000

    get the source code

--- natas10/natas10.py
+++ natas10/natas10.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
 import re
+import html
 
 
 # configuration for natas10
@@ -15,3 +16,9 @@ session.auth = AUTH
 response = session.get(URL)
 div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
 print(f'{div_content}\n')
+
+
+response = session.get(URL + 'index-source.html')
+source = html.unescape(response.text).replace('<br />', '')
+div = bs4.BeautifulSoup(source, 'lxml').body.div
+print(f'{div}\n')
