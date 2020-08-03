37098b2b4a54adcd90a0dba2777e7d326379b693
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-03 12:28:34 +1000

    get the body of the challenge

--- natas11/natas11.py
+++ natas11/natas11.py
@@ -6,5 +6,12 @@ import re
 # configuration for natas11
 USERNAME = 'natas11'
 PASSWORD = 'U82q5TCMMQ9xuFoI3dYX61s7OZD9JKoK'
-URL = 'http://natas10.natas.labs.overthewire.org/'
+URL = 'http://natas11.natas.labs.overthewire.org/'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
+session = requests.Session()
+session.auth = AUTH
+response = session.get(URL)
+body = bs4.BeautifulSoup(response.text, 'html.parser').body
+print(f'{body}\n')
