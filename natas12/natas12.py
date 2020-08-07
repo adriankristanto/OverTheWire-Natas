9415c74baccdc3afd62a0bf04a5b904c46fea3e8
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-07 21:36:56 +1000

    added a simple explanation of the source code

--- natas12/natas12.py
+++ natas12/natas12.py
@@ -22,3 +22,62 @@ print(f'{div_content}\n')
 response = session.get(URL + 'index-source.html')
 source = html.unescape(response.text).replace('<br />', '')
 print(source)
+
+"""
+<? 
+
+# here, the function generates a random string of length 10 using Mersenne Twister RNG
+function genRandomString() {
+    $length = 10;
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+    $string = "";    
+
+    for ($p = 0; $p < $length; $p++) {
+        $string .= $characters[mt_rand(0, strlen($characters)-1)];
+    }
+
+    return $string;
+}
+
+# here, we generate a random path using the dir parameter and the above function
+function makeRandomPath($dir, $ext) {
+    do {
+    $path = $dir."/".genRandomString().".".$ext;
+    } while(file_exists($path));
+    return $path;
+}
+
+# here, we get the extension info of our file and generate random path for our file
+# reference: https://www.php.net/manual/en/function.pathinfo.php
+function makeRandomPathFromFilename($dir, $fn) {
+    $ext = pathinfo($fn, PATHINFO_EXTENSION);
+    return makeRandomPath($dir, $ext);
+}
+
+if(array_key_exists("filename", $_POST)) {
+    # create a random path: upload/randomstring.ext
+    $target_path = makeRandomPathFromFilename("upload", $_POST["filename"]);
+
+    # reference: https://www.php.net/manual/en/reserved.variables.files.php
+    # array of items uploaded to the current script via the HTTP POST method
+    if(filesize($_FILES['uploadedfile']['tmp_name']) > 1000) {
+        echo "File is too big";
+    } else {
+        if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+            echo "The file <a href=\"$target_path\">$target_path</a> has been uploaded";
+        } else{
+            echo "There was an error uploading the file, please try again!";
+        }
+    }
+} else {
+?>
+
+<form enctype="multipart/form-data" action="index.php" method="POST">
+<input type="hidden" name="MAX_FILE_SIZE" value="1000" />
+<input type="hidden" name="filename" value="<? print genRandomString(); ?>.jpg" />
+Choose a JPEG to upload (max 1KB):<br/>
+<input name="uploadedfile" type="file" /><br />
+<input type="submit" value="Upload File" />
+</form>
+<? } ?> 
+"""
