ee1aa3e785be3ba8685260d65e9b29b17279837a
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-12 20:38:19 +1000

    explained the source and the attack to be performed

--- natas14/natas14.py
+++ natas14/natas14.py
@@ -21,3 +21,28 @@ print(f'{div_content}\n')
 # get the source code
 response = session.get(URL + 'index-source.html')
 print(response.text)
+
+
+"""
+<?
+if(array_key_exists("username", $_REQUEST)) {
+    $link = mysql_connect('localhost', 'natas14', '<censored>');
+    mysql_select_db('natas14', $link);
+    
+    $query = "SELECT * from users where username=\"".$_REQUEST["username"]."\" and password=\"".$_REQUEST["password"]."\"";
+    if(array_key_exists("debug", $_GET)) {
+        echo "Executing query: $query<br>";
+    }
+
+    if(mysql_num_rows(mysql_query($query, $link)) > 0) {
+            echo "Successful login! The password for natas15 is <censored><br>";
+    } else {
+            echo "Access denied!<br>";
+    }
+    mysql_close($link);
+} else {
+?> 
+"""
+# as we can see in the source code, the user inputs, which include the username and the password, are immediately 
+# passed on to the sql query with no sanitisation
+# therefore, we can perform a SQL injection attack
