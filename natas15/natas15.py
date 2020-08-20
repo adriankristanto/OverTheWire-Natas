17efceacdfe533fb6e6c98eb28f1a9ead9f1c5ce
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-20 13:37:32 +1000

    explained the attack

--- natas15/natas15.py
+++ natas15/natas15.py
@@ -15,3 +15,39 @@ session.auth = AUTH
 response = session.get(URL)
 div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
 print(f'{div_content}\n')
+
+
+# get the source code
+# print(session.get(URL + 'index-source.html').text)
+# from the source code, we can see that there is a database table with 2 properties, which are
+# username and password
+"""
+/*
+CREATE TABLE `users` (
+  `username` varchar(64) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL
+);
+*/ 
+"""
+
+
+# explaining the attack
+# case 1: this user doesn't exist
+# the page will display "this user doesn't exist" when the username is not in the database
+data1 = {
+    'username' : 'randomusername',
+    'submit' : 'submit'
+}
+response = session.post(URL, data=data1)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
+# case 2: this user exists
+# the page will only display "this user exists" when the username is in the database
+data2 = {
+    'username' : 'natas16',
+    'submit' : 'submit'
+}
+response = session.post(URL, data=data2)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
+# the attack that we can perform here is a blind SQL injection attack
