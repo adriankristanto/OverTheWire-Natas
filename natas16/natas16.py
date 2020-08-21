cfc4c0c33ea2bd49dcdc487e905ff2764605a3f3
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-21 17:20:53 +1000

    start with natas16 -> natas17

---
+++ natas16/natas16.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas16
+USERNAME = 'natas16'
+PASSWORD = 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'
+URL = 'http://natas15.natas.labs.overthewire.org/'
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+
+
