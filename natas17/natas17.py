d1cfa336579b75953a9214c4c45a09711205a6c5
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-26 20:25:46 +1000

    explained the attack

--- natas17/natas17.py
+++ natas17/natas17.py
@@ -21,3 +21,20 @@ print(f'{div_content}\n')
 # get the source code
 response = session.get(URL + 'index-source.html')
 # print(response.text)
+
+
+# as we can see in the source, the feedback to the user is now commented out
+"""
+if(mysql_num_rows($res) > 0) {
+        //echo "This user exists.<br>";
+    } else {
+        //echo "This user doesn't exist.<br>";
+    }
+    } else {
+        //echo "Error in query.<br>";
+    }
+"""
+# one attack that we can try is time-based SQL injection attack
+# the idea is that if the user exists, then the website will return its response 
+# after X seconds
+# otherwise, the website will response immediately
