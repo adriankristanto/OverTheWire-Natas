93b9a47f50f9dfb12518ec3de1ee721f7228b58a
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-02 21:42:33 +1000

    fixed spacing

--- natas18/natas18.py
+++ natas18/natas18.py
@@ -59,6 +59,7 @@ for i in range(MIN, MAX+1):
         admin_page = response.text
         break
 
+
 # natas19 password: 4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs
 password = re.search(r'Password: (\w+)', admin_page)[1]
 print(f'natas19 password: {password}')
