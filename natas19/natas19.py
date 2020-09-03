abd82d08d542642f28d97ac0cd9919af96c1ba21
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-03 19:55:44 +1000

    get the cookie from the webserver

--- natas19/natas19.py
+++ natas19/natas19.py
@@ -16,3 +16,16 @@ session.auth = AUTH
 response = session.get(URL)
 div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
 print(f'{div_content}\n')
+
+
+# as the webpage said, the code used is the same as the previous level
+# however, the session ids are not sequential
+# let's see what this means
+data = {
+    'username' : 'randomusername',
+    'password' : 'randompassword',
+    'submit' : 'submit'
+}
+response = session.post(URL, data=data)
+# example id: PHPSESSID=3433302d72616e646f6d757365726e616d65
+print(response.cookies)
