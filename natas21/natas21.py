49051e510d01734c23df60c4718b991bf89a8bfd
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-07 22:03:45 +1000

    get the challenge content

--- natas21/natas21.py
+++ natas21/natas21.py
@@ -12,3 +12,7 @@ AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 
 session = requests.Session()
 session.auth = AUTH
+# get the challenge content
+response = session.get(URL)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
