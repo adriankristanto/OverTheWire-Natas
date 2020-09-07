bc2c10185fac8abbbf3aa7c69a9a2221b770fa62
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-07 22:01:39 +1000

    start with natas21 -> natas22

---
+++ natas21/natas21.py
@@ -0,0 +1,10 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas21
+USERNAME = 'natas21'
+PASSWORD = 'IFekPyrQXftziDEsUr3x21sYuahypdgJ'
+URL = 'http://natas21.natas.labs.overthewire.org/'
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
