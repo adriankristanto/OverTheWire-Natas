1c615d8573743bf12c60ff27168787096d3014cd
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-09 22:49:08 +1000

    explained the first part of the source code

--- natas22/natas22.py
+++ natas22/natas22.py
@@ -21,3 +21,22 @@ print(f'{div_content}\n')
 # get the source code
 # response = session.get(URL + 'index-source.html')
 # print(response.text)
+
+
+# in the source code,
+"""
+<?
+session_start();
+
+if(array_key_exists("revelio", $_GET)) {
+    // only admins can reveal the password
+    if(!($_SESSION and array_key_exists("admin", $_SESSION) and $_SESSION["admin"] == 1)) {
+    header("Location: /");
+    }
+}
+?> 
+"""
+# at the start, if we use a get request and include the revelio parameter, it will 
+# check whether we are the admin or not
+# if we are not the admin, we will get redirected to the root directory
+# this is shown when the server returns Location: / in the header
