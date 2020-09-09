4880c17a69278656116e21aac7f2d28d8ee516ca
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-09 23:45:19 +1000

    explained the source code

--- natas23/natas23.py
+++ natas23/natas23.py
@@ -24,3 +24,26 @@ response = session.get(URL + 'index-source.html')
 source = html.unescape(response.text).replace('<br />', '')
 source = bs4.BeautifulSoup(source, 'lxml')
 # print(source.prettify())
+
+
+"""
+<?php
+    if(array_key_exists("passwd",$_REQUEST)){
+        if(strstr($_REQUEST["passwd"],"iloveyou") && ($_REQUEST["passwd"] > 10 )){
+            echo "<br>The credentials for the next level are:<br>";
+            echo "<pre>Username: natas24 Password: <censored></pre>";
+        }
+        else{
+            echo "<br>Wrong!<br>";
+        }
+    }
+    // morla / 10111
+?>  
+"""
+# firstly, once we made a requests with passwd as one of the params,
+# we need to include "iloveyou" string in the passwd as 
+# the strstr() function will return everything up to the needle
+# if the string doesn't exist, it will return False
+# reference: https://www.php.net/manual/en/function.strstr.php
+# next, it will compare the string that we submitted with the integer 10
+# if the string is greater than 10, it will return True and we will get the admin page
