44c2d008ccd50d467b3ae37e4bde78f736776cc4
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-12 20:51:09 +1000

    explained the source and the php strcmp function vulnerability

--- natas24/natas24.py
+++ natas24/natas24.py
@@ -24,3 +24,26 @@ response = session.get(URL + 'index-source.html')
 source = html.unescape(response.text).replace('<br />', '')
 source = bs4.BeautifulSoup(source, 'lxml')
 # print(source.prettify())
+
+
+"""
+<?php
+    if(array_key_exists("passwd",$_REQUEST)){
+        if(!strcmp($_REQUEST["passwd"],"<censored>")){
+            echo "<br>The credentials for the next level are:<br>";
+            echo "<pre>Username: natas25 Password: <censored></pre>";
+        }
+        else{
+            echo "<br>Wrong!<br>";
+        }
+    }
+    // morla / 10111
+?>  
+"""
+# in the source code, we need to know the password to get the credentials for the 
+# next level
+# however, php's strcmp() function has a known vulnerability
+# although the implementation is correct, (strcmp() will return 0 when the strings match)
+# when comparing strings and non-string objects, it will return unexpected values
+# reference: https://www.php.net/manual/en/function.strcmp.php
+# for example, strcmp("foo", array()) => NULL + PHP Warning
