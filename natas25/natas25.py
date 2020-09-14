21f58805fa2c13dd4da37fd24dbead2ee13534ff
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-15 00:19:02 +1000

    get the source code

--- natas25/natas25.py
+++ natas25/natas25.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
 import re
+import html
 
 
 # configuration for natas25
@@ -16,3 +17,8 @@ session.auth = AUTH
 response = session.get(URL)
 div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
 print(f'{div_content}\n')
+
+
+# get the source code
+response = session.get(URL + 'index-source.html')
+# print(html.unescape(response.text).replace('<br />', ''))
