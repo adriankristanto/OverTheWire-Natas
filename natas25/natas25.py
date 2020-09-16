300a97a9e0be1b45b0a3a4d323a85dda581ffb19
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-16 19:24:33 +1000

    explained the first step of the attack

--- natas25/natas25.py
+++ natas25/natas25.py
@@ -22,3 +22,45 @@ print(f'{div_content}\n')
 # get the source code
 response = session.get(URL + 'index-source.html')
 # print(html.unescape(response.text).replace('<br />', ''))
+
+
+# first part
+"""
+function setLanguage(){
+    /* language setup */
+    if(array_key_exists("lang",$_REQUEST))
+        if(safeinclude("language/" . $_REQUEST["lang"] ))
+            return 1;
+    safeinclude("language/en"); 
+}
+
+function safeinclude($filename){
+    // check for directory traversal
+    if(strstr($filename,"../")){
+        logRequest("Directory traversal attempt! fixing request.");
+        $filename=str_replace("../","",$filename);
+    }
+    // dont let ppl steal our passwords
+    if(strstr($filename,"natas_webpass")){
+        logRequest("Illegal file access detected! Aborting!");
+        exit(-1);
+    }
+    // add more checks...
+
+    if (file_exists($filename)) { 
+        include($filename);
+        return 1;
+    }
+    return 0;
+}
+"""
+# in the source code, there is a safeinclude()
+# which give us a hint of the type of the vulnerability
+# which is directory traversal because of insufficient
+# user input sanitisation
+# for example, in this case, we can perform "unsafe string replacement attack"
+# reference: https://nets.ec/Unsafe_string_replacement
+# since the server will replace every occurence of ../ with "",
+# with the input "....//", we can bypass the input sanitisation
+# so, when "../" is removed from "....//", we stilll get "../"
+# which still allow for directory traversal
