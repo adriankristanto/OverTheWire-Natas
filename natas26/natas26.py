590a42010ad9b27a20d8fee0c1a3d8e2611f5854
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-20 15:36:51 +1000

    start with natas26 -> natas27

---
+++ natas26/natas26.py
@@ -0,0 +1,10 @@
+import requests
+import bs4
+import re
+
+
+# configuration for natas26
+USERNAME = 'natas26'
+PASSWORD = 'oGgWAJ7zcGT28vYazGo4rkhOPDhBu34T'
+AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+URL = 'http://natas26.natas.labs.overthewire.org/'
