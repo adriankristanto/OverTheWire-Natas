098b6837d0a49bc409a03630ba32501377cf2b55
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-24 15:08:12 +1000

    draw a straight line by using the web page the way it supposed to be used

--- natas26/natas26.py
+++ natas26/natas26.py
@@ -21,3 +21,16 @@ print(f'{div_content}\n')
 # get the source code
 response = session.get(URL + 'index-source.html')
 # print(response.text)
+
+
+# firstly, let's try to use the feature provided by the web server, 
+# which is drawing a straight line
+params = {
+    'x1' : 50,
+    'y1' : 50,
+    'x2' : 100,
+    'y2' : 100
+}
+response = session.get(URL, params=params)
+div_content = bs4.BeautifulSoup(response.text, 'html.parser').body.find('div', {'id' : 'content'})
+print(f'{div_content}\n')
