b5b9114d450ca5c7e875d629fb5d6ae39a982e4a
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-26 20:22:23 +1000

    set the session for the challenge

--- natas27/natas27.py
+++ natas27/natas27.py
@@ -8,3 +8,6 @@ USERNAME = 'natas27'
 PASSWORD = 'oGgWAJ7zcGT28vYazGo4rkhOPDhBu34T'
 AUTH = requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
 URL = 'http://natas27.natas.labs.overthewire.org/'
+
+session = requests.Session()
+session.auth = AUTH
